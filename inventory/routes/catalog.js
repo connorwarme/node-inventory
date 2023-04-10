4580d22980a3ac39d03366474dd094f2d5aca667
connorwarme <connor.warme@gmail.com>
2023-04-09 21:29:52 -0600

    Start building routes on catalog

---
+++ inventory/routes/catalog.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const router = express.Router()
+
+// require controller modules
+const brand_controller = require("../controllers/brandController")
+const category_controller = require("../controllers/categoryController")
+const goggle_controller = require("../controllers/goggleController")
+const goggleinstance_controller = require("../controllers/goggleInstanceController")
+const tag_controller = require("../controllers/tagController")
+
