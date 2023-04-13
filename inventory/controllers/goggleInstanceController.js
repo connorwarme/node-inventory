ed1cdd96423199ec69aa3dbd8db92c198404baa8
connorwarme <connor.warme@gmail.com>
2023-04-12 19:33:51 -0600

    Build out goggleinstance create form controller

--- inventory/controllers/goggleInstanceController.js
+++ inventory/controllers/goggleInstanceController.js
@@ -1,6 +1,7 @@
 const GoggleInstance = require("../models/goggleinstance")
 const Goggle = require("../models/goggle")
 const async = require("async")
+const { body, validationResult } = require("express-validator")
 
 // display all goggleinstances
 exports.goggleinstance_list = (req, res, next) => {
@@ -64,14 +65,74 @@ exports.goggleinstance_detail = (req, res, next) => {
 }
 
 // display goggleinstance create form on GET
-exports.goggleinstance_create_get = (req, res) => {
-  res.send("Not Implemented Yet: goggleinstance create GET");
+exports.goggleinstance_create_get = (req, res, next) => {
+  Goggle.find({}, "name").exec((err, goggles) => {
+    if (err) {
+      return next(err)
+    }
+    res.render("goggleinstance_form", {
+      title: "Create Goggle + Lens",
+      goggle_list: goggles,
+    })
+  })
 }
 
 // handle goggleinstance create on POST
-exports.goggleinstance_create_post = (req, res) => {
-  res.send("Not Implemented Yet: goggleinstance create POST");
-}
+exports.goggleinstance_create_post = [
+  body("goggle", "Goggle must be specified")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("lens.style", "Goggle lens style must be declared")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("lens.detail", "Goggle lens details required")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("quantity", "Specify quantity available")
+    .trim()
+    .isLength({ min: 1 })
+    .isInt({ gt: 0, lt: 100 })
+    .withMessage("Quantity must be a number between 1-100")
+    .escape(),
+
+  (req, res, next) => {
+    const errors = validationResult(req)
+
+    const goggleinstance = new GoggleInstance({
+      goggle: req.body.goggle,
+      lens: {
+        style: req.body.lens.style,
+        detail: req.body.lens.detail,
+      },
+      quantity: req.body.quantity,
+    })
+
+    if (!errors.isEmpty()) {
+      Goggle.find({}, "name").exec(function(err, goggles) {
+        if (err) {
+          return next(err)
+        }
+        res.render("goggleinstance_form", {
+          title: "Create Goggle + Lens",
+          goggle_list: goggles,
+          selected_goggle: goggleinstance.goggle._id,
+          errors: errors.array(),
+          goggleinstance,
+        })
+      })
+      return;
+    }
+    goggleinstance.save((err) => {
+      if (err) {
+        return next(err);
+      }
+      res.redirect(goggleinstance.url)
+    })
+  }
+]
 
 // display goggleinstance delete form on GET
 exports.goggleinstance_delete_get = (req, res) => {
