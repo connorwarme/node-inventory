b335cce8f847f8a793c5497f820871e556a61180
connorwarme <connor.warme@gmail.com>
2023-04-16 16:29:10 -0600

    Add update for goggle instances to controller

--- inventory/controllers/goggleInstanceController.js
+++ inventory/controllers/goggleInstanceController.js
@@ -160,11 +160,88 @@ exports.goggleinstance_delete_post = (req, res, next) => {
 }
 
 // display goggleinstance update form on GET
-exports.goggleinstance_update_get = (req, res) => {
-  res.send("Not Implemented Yet: goggleinstance update GET");
+exports.goggleinstance_update_get = (req, res, next) => {
+  async.parallel(
+    {
+      instance(callback) {
+        GoggleInstance.findById(req.params.id).exec(callback)
+      },
+      goggle(callback) {
+        Goggle.find({}, "name").exec(callback)
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err)
+      }
+      if (results.instance == null) {
+        const error = new Error("That goggle + lens combo was not found in the database")
+        error.status = 404
+        return next(error)
+      }
+      res.render("goggleinstance_form", {
+        title: "Update Goggle + Lens Combo",
+        instance: results.instance,
+        goggle_list: results.goggle,
+      })
+    }
+  )
 }
 
 // handle goggleinstance update on POST
-exports.goggleinstance_update_post = (req, res) => {
-  res.send("Not Implemented Yet: goggleinstance update POST");
-}
+exports.goggleinstance_update_post = [
+  body("goggle", "Goggle must be specified")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("style", "Goggle lens style must be declared")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("detail", "Goggle lens details required")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("quantity", "Specify quantity available")
+    .trim()
+    .isLength({ min: 1 })
+    .isInt({ gt: 0, lt: 100 })
+    .withMessage("Quantity must be a number between 1-100")
+    .escape(),
+
+  (req, res, next) => {
+    const errors = validationResult(req)
+
+    const goggleinstance = new GoggleInstance({
+      goggle: req.body.goggle,
+      lens: {
+        style: req.body.style,
+        detail: req.body.detail,
+      },
+      quantity: req.body.quantity,
+      _id: req.params.id,
+    })
+
+    if (!errors.isEmpty()) {
+      Goggle.find({}, "name").exec(function(err, goggles) {
+        if (err) {
+          return next(err)
+        }
+        res.render("goggleinstance_form", {
+          title: "Create Goggle + Lens",
+          goggle_list: goggles,
+          selected_goggle: goggleinstance.goggle._id,
+          errors: errors.array(),
+          instance: goggleinstance,
+        })
+      })
+      return;
+    }
+    GoggleInstance.findByIdAndUpdate(req.params.id, goggleinstance, (err) => {
+      if (err) {
+        return next(err);
+      }
+      res.redirect(goggleinstance.url)
+    })
+  }
+]
