3a8427eff4998bbdef9d08b7f86b21692e6fff88
connorwarme <connor.warme@gmail.com>
2023-04-16 16:15:30 -0600

    Add update to goggle controller and view

--- inventory/controllers/goggleController.js
+++ inventory/controllers/goggleController.js
@@ -259,11 +259,129 @@ exports.goggle_delete_post = (req, res, next) => {
 }
 
 // display goggle update form on GET
-exports.goggle_update_get = (req, res) => {
-  res.send("Not Implemented Yet: goggle update GET");
+exports.goggle_update_get = (req, res, next) => {
+  async.parallel(
+    {
+      goggle(callback) {
+        Goggle.findById(req.params.id)
+        .populate("brand")
+        .populate("category")
+        .populate("tag")
+        .exec(callback)
+      },
+      brand(callback) {
+        Brand.find().exec(callback)
+      },
+      category(callback) {
+        Category.find().exec(callback)
+      },
+      tag(callback) {
+        Tag.find().exec(callback)
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err)
+      }
+      if (results.goggle == null) {
+        const error = new Error("Goggle model not found in database")
+        error.status = 404
+        return next(error)
+      }
+      res.render("goggle_form", {
+        title: "Update Goggle",
+        goggle: results.goggle,
+        brands: results.brand,
+        categories: results.category,
+        tags: results.tag,
+      })
+    }
+  )
 }
 
 // handle goggle update on POST
-exports.goggle_update_post = (req, res) => {
-  res.send("Not Implemented Yet: goggle update POST");
-}
+exports.goggle_update_post = [
+  (req, res, next) => {
+    if (!Array.isArray(req.body.tag)) {
+      req.body.tag = typeof req.body.tag === "undefined" ? [] : [req.body.tag]
+    }
+    next();
+  },
+  body("name", "Goggle model must have a name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("brand", "Goggle brand must be specified")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("description", "Goggle needs a description")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("category", "Goggle category must be specified")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("cost", "Cost of goggle must be declared")
+    .trim()
+    .isInt({ min: 50, max: 325 })
+    .withMessage("Cost must be an integer between 50 and 325")
+    .escape(),
+  body("tag.*").escape(),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    const goggle = new Goggle({ 
+      name: req.body.name,
+      brand: req.body.brand,
+      description: req.body.description,
+      category: req.body.category,
+      cost: req.body.cost,
+      tag: req.body.tag,
+      _id: req.params.id,
+    })
+
+    if (!errors.isEmpty()) {
+      async.parallel(
+        {
+          brands(callback) {
+            Brand.find(callback)
+          },
+          categories(callback) {
+            Category.find(callback)
+          },
+          tags(callback) {
+            Tag.find(callback)
+          },
+        },
+        (err, results) => {
+          if (err) {
+            return next(err)
+          }
+          for (const tag of results.tags) {
+            if (goggle.tag.includes(tag._id)) {
+              tag.checked = "true";
+            }
+          }
+          res.render("goggle_form", {
+            title: "Create Goggle",
+            brands: results.brands,
+            categories: results.categories,
+            tags: results.tags,
+            goggle,
+            errors: errors.array(),
+          })
+        }
+      )
+      return;
+    }
+    Goggle.findByIdAndUpdate(req.params.id, goggle, (err) => {
+      if (err) {
+        return next(err)
+      }
+      res.redirect(goggle.url)
+    })
+  }
+]
